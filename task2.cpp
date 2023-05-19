4547cf4deca189bc9c50ccf66c295761cb46d96a
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-05-19 23:18:04 +1100

    work version

--- task2.cpp
+++ task2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <ctime>
 
+void painting_size_initialization(std::vector <std::vector <int>>& vec, int& rows, int& cols) {	
 
-int main() {
-
-	int rows;
-	int cols ;
+	std::srand(std::time(nullptr));
 
 	std::cout << "Enter the number of rows: ";
 	std::cin >> rows;
@@ -13,33 +13,55 @@ int main() {
 	std::cout << "Enter the number of cols: ";
 	std::cin >> cols;
 
-
-	std::vector <std::vector <int>> vec; 
-
 	for (int i = 0; i < rows; ++i) {
 
-		std::vector <int> temp;
+		std::vector <int> vec_cols;
 
 		for (int j = 0; j < cols; ++j) {
 
-			temp.push_back(rand() % 2);
+			vec_cols.push_back(rand() % 2);
 
 		}
 
-		vec.push_back(temp);
+		vec.push_back(vec_cols);
 
-	}
+	}	
+}
 
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < cols; ++j) {
+int main() {
 
-			std::cout << vec[i][j];
-
-		}
-		std::cout << std::endl;
-	}
-
-
+	int rows;
+	int cols;	
 	
+	std::vector <std::vector <int>> vec; 	
 
+	painting_size_initialization(vec, rows, cols);
+
+	std::ofstream picture("D:\\Skillbox\\C++\\working\\Practice 20 (Writing to files)\\task2\\pic.txt");
+	
+	if (picture.is_open()) {
+
+		std::cout << "File is open\n";		
+
+		for (int i = 0; i < rows; ++i) {
+
+			for (int j = 0; j < cols; ++j) {
+
+				picture << vec[i][j];
+
+			}				
+
+			picture << std::endl;
+
+		}
+	}
+	else {
+
+		std::cout << "WARNING!\n";
+		std::cout << "ERROR!!!\n";
+		std::cout << "File is not open\n";
+
+	}	
+		
+	picture.close();
 }
